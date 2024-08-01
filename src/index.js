cd62c569997fc46155b213121b3cc5d3497b806b
Alec Lang <lostlang@icloud.com>
2024-08-01 14:24:26 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+	if (number === 0) {
+		return "zero";
+	}
+	let out = [];
+	out.push(thirdDigit(Math.floor(number / 100)));
+	number %= 100;
+	if (Math.floor(number / 10) === 1) {
+		out.push(firstTwoDigit(number % 10));
+	} else {
+		out.push(secondDigit(Math.floor(number / 10)));
+		out.push(firstDigit(number % 10));
+	}
+	return out.filter((el) => el).join(" ");
+};
+
+function firstDigit(number) {
+	return [
+		"",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	][number];
+}
+
+function secondDigit(number) {
+	return [
+		"",
+		"",
+		"twenty",
+		"thirty",
+		"forty",
+		"fifty",
+		"sixty",
+		"seventy",
+		"eighty",
+		"ninety",
+	][number];
+}
+
+function firstTwoDigit(number) {
+	return [
+		"ten",
+		"eleven",
+		"twelve",
+		"thirteen",
+		"fourteen",
+		"fifteen",
+		"sixteen",
+		"seventeen",
+		"eighteen",
+		"nineteen",
+	][number];
+}
+
+function thirdDigit(number) {
+	return [
+		"",
+		"one hundred",
+		"two hundred",
+		"three hundred",
+		"four hundred",
+		"five hundred",
+		"six hundred",
+		"seven hundred",
+		"eight hundred",
+		"nine hundred",
+	][number];
 }
